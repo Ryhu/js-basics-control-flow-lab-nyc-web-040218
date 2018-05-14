0818aa248f2cee58047d612f5fc6d88e7f0842bc
Ryan Hu <ryanhuuu@gmail.com>
2018-05-14 17:51:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ return a;
 function switchOnCharmFromTip(tip){
   if (tip == "generous")
     return "Thank you so much.";
-  else (if tip == "not so generous")
+  else if (tip == "not so generous")
     return "Thank you.";
   else 
     return "Bye.";
