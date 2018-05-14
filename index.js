3f3174f3d469aa9b692424ca816008c8c3a9aad8
Ryan Hu <ryanhuuu@gmail.com>
2018-05-14 17:30:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function scuberGreetingForFeet(feet){
   if (feet < 400)
     return 'This one is on me!';
   else if (feet > 2500)
-      return 'I will gladly take your thirty bucks.';
+      return ''No can do.'';
   else if (feet > 2000)
       return 'I will gladly take your thirty bucks.';
     
