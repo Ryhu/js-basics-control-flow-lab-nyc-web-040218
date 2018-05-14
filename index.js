d86cde3bf9b098a08d69662d7497f1d4453a4af3
Ryan Hu <ryanhuuu@gmail.com>
2018-05-14 17:22:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ function scuberGreetingForFeet(feet){
       break;
       
   }
-
-    
 } 
+
 //— Use if and else if statements to return the correct greeting based on the distance the passenger desires to travel.
 // ternaryCheckCity() — Use a ternary operator to return the correct response based on the desired destination of the passenger.
 // switchOnCharmFromTip() — Use a switch statement to return a different response based on the generosity of the passenger's tip.
