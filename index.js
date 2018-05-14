cc038e50673f1d285c5b5e81d6d04a7781b83139
Ryan Hu <ryanhuuu@gmail.com>
2018-05-14 17:45:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function scuberGreetingForFeet(feet){
 function ternaryCheckCity(city){
 a = ""
 city == 'nyc' ? a =  "Ok, sounds good." : a =  "No go.";
+return a
 } 
 //— Use a ternary operator to return the correct response based on the desired destination of the passenger.
 // switchOnCharmFromTip() — Use a switch statement to return a different response based on the generosity of the passenger's tip.
